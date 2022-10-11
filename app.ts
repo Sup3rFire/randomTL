515831939541df1abf28e573cfed578ff97c3a10
Super Fire <36259493+Sup3rFire@users.noreply.github.com>
2022-10-11 09:57:44 +0700

    Get players from players.csv

--- app.ts
+++ app.ts
@@ -12,21 +12,9 @@ async function getReplayIds(userId: string): Promise<string[]> {
   ).data.records.map((record: { replayid: string }) => record.replayid);
 }
 
-const idList = [
-  "5fb0b7183a6a8b21b897e660", // kez
-  "5e7cbb652932b46c9c671ce4", // diao
-  "5e844b0868270e617d52c990", // cz
-  "5e436daf368a5d2489fbf848", // star
-  "5eb11da1e47b643b5d12e5f1", // kazu
-  "5f5dbcc4454e942b4fdfc5fa", // vonce
-  "5e697bbf77aba60f95fb38da", // fs
-  "5e3f6ebe3dacc16dbc4dd6aa", // doremy
-  "6049c31e25f490b0cb7559c4", // feesh
-  "5ed890931bba6a519bfd4db4", // frenZ
-  "5f19118f03a6e76c187c9a69", // amka
-  "5ed99d387dd5287740926a6d", // akrin
-  "615fb20fe17beeef45104302", // ballrg
-];
+const idList = Deno.readTextFileSync("players.csv")
+  .split("\n")
+  .map((player) => player.split(",")[0]);
 
 let randomId = getRandomValue(idList);
 
